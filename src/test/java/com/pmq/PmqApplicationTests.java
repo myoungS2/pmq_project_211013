dcb4eb2561df4547e827ce762a834e69a34ffae3
D:\웹개발(21-05-20) <mythe1004@naver.com>
2021-10-16 15:35:32 +0900

    21-10-16 환경세팅

---
+++ src/test/java/com/pmq/PmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
